f7c43ab341ec7db16be421856145f14e9717551a
KennySo <HoyanSo1996@163.com>
2025-02-26 11:10:34 +0800

    创建 服务消费者模块.

---
+++ user-service-consumer-01/src/main/java/com/omega/springcloud/UserServiceConsumerApplication01.java
@@ -0,0 +1,12 @@
+package com.omega.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserServiceConsumerApplication01 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserServiceConsumerApplication01.class, args);
+    }
+}
---
+++ user-service-consumer-01/src/main/java/com/omega/springcloud/controller/UserController.java
@@ -0,0 +1,37 @@
+package com.omega.springcloud.controller;
+
+import com.omega.springcloud.entity.Result;
+import com.omega.springcloud.entity.User;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import javax.annotation.Resource;
+
+@Slf4j
+@RestController
+@RequestMapping("/consumer/user")
+public class UserController {
+
+    private static final String USER_SERVICE_PROVIDER_URL = "http://localhost:8081";
+
+    @Resource
+    private RestTemplate restTemplate;
+
+    @RequestMapping("/query/{id}")
+    public Result query(@PathVariable("id") Long id) {
+        log.info("消费者01 接收到的 id = {}", id);
+        String url = USER_SERVICE_PROVIDER_URL + "/provider/user/query/" + id;
+        return restTemplate.getForObject(url, Result.class);
+    }
+
+    @RequestMapping("/save")
+    public Result save(@RequestBody User user) {
+        log.info("消费者01 接收到的 user = {}", user);
+        String url = USER_SERVICE_PROVIDER_URL + "/provider/user/save";
+        return restTemplate.postForObject(url, user, Result.class);
+    }
+}
