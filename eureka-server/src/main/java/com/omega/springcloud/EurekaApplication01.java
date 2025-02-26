70af0ddba2d5b79981f6935b58fdc8017402d60b
KennySo <HoyanSo1996@163.com>
2025-02-26 11:24:54 +0800

    [Eureka] 创建 Eureka Server 模块.

---
+++ eureka-server/src/main/java/com/omega/springcloud/EurekaApplication01.java
@@ -0,0 +1,14 @@
+package com.omega.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer  // 声明 Eureka Server
+@SpringBootApplication
+public class EurekaApplication01 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication01.class, args);
+    }
+}
