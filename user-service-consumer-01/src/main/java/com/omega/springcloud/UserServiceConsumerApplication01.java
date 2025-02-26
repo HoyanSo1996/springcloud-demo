ef963d59c401ead1469e8d29e8b5f4af034851fb
KennySo <HoyanSo1996@163.com>
2025-02-26 11:42:10 +0800

    [Eureka] 在 服务生产者&消费者模块 上集成 Eureka Client. (服务注册)

--- user-service-consumer-01/src/main/java/com/omega/springcloud/UserServiceConsumerApplication01.java
+++ user-service-consumer-01/src/main/java/com/omega/springcloud/UserServiceConsumerApplication01.java
@@ -2,7 +2,9 @@ package com.omega.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient // 开启 eureka client
 @SpringBootApplication
 public class UserServiceConsumerApplication01 {
 
--- user-service-provider-01/src/main/java/com/omega/springcloud/UserServiceProviderApplication01.java
+++ user-service-provider-01/src/main/java/com/omega/springcloud/UserServiceProviderApplication01.java
@@ -3,7 +3,9 @@ package com.omega.springcloud;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient // 开启 eureka client
 @SpringBootApplication
 @MapperScan("com.omega.springcloud.mapper")
 public class UserServiceProviderApplication01 {
