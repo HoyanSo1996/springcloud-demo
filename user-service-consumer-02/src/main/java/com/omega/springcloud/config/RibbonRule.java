a957ebc10a1318081e05ba25cf45339c01ac3b6d
KennySo <HoyanSo1996@163.com>
2025-05-26 18:14:28 +0800

    [Nacos] 配置 指定的负载均衡算法.

---
+++ user-service-consumer-02/src/main/java/com/omega/springcloud/config/RibbonRule.java
@@ -0,0 +1,18 @@
+package com.omega.springcloud.config;
+
+// import com.netflix.loadbalancer.IRule;
+// import com.netflix.loadbalancer.RandomRule;
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Configuration;
+//
+// @Configuration
+// public class RibbonRule {
+//
+//    /**
+//     * 指定 轮询算法
+//     */
+//    @Bean
+//    public IRule myRibbonRule() {
+//        return new RandomRule();
+//    }
+// }
