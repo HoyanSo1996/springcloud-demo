2c2119e8387a23fd17b9fee031adf003791a9d95
Kenny82005570 <Kenny.HX.Su@pccw.com>
2025-06-04 18:09:26 +0800

    [Nacos] 配置 热更新.(配置中心)

--- user-service-consumer-02/src/main/java/com/omega/springcloud/config/NacosConfig.java
+++ user-service-consumer-02/src/main/java/com/omega/springcloud/config/NacosConfig.java
@@ -2,8 +2,10 @@ package com.omega.springcloud.config;
 
 import lombok.Data;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Configuration;
 
+@RefreshScope // 方式一: 实现配置信息自动刷新 (是 springcloud 的原生注解)
 @Configuration
 @Data
 public class NacosConfig {
@@ -14,3 +16,14 @@ public class NacosConfig {
     @Value("${config.password}")
     private String password;
 }
+
+
+// @ConfigurationProperties(prefix = "config")  // 方式二: 指定配置信息的前缀名
+// @Configuration
+// @Data
+// public class NacosConfig {
+//
+//     private String username;
+//
+//     private String password;
+// }
