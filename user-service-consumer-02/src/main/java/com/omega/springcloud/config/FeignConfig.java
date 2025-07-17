1b3e522ae9c67b4bd4f819efd0296b7ce0bd48a9
KennySo <Hoyanso1996@163.com>
2025-07-17 18:32:06 +0800

    [OpenFeign] 配置 日志信息.

---
+++ user-service-consumer-02/src/main/java/com/omega/springcloud/config/FeignConfig.java
@@ -0,0 +1,12 @@
+package com.omega.springcloud.config;
+
+// @Configuration
+// public class FeignConfig {
+//
+//
+//     @Bean
+//     // @ConditionalOnClass(UserWebService.class)  // 如果为加这个就是全局配置，如果加了就是为某个指定的客户端配置
+//     public Logger.Level feignLoggerLevel() {
+//         return Logger.Level.FULL;
+//     }
+// }
