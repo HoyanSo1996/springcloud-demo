c1acb491a4083d4d50650a39ce0705eb9518266e
Kenny82005570 <Kenny.HX.Su@pccw.com>
2025-08-14 17:15:39 +0800

    fix(OpenFeign): 修正接口消息提示.

--- user-service-provider-04/src/main/java/com/omega/springcloud/controller/UserController.java
+++ user-service-provider-04/src/main/java/com/omega/springcloud/controller/UserController.java
@@ -42,11 +42,11 @@ public class UserController {
 
     @GetMapping("/find/{id}")
     public Result<User> findWithRequestHeader(@PathVariable("id") Long id, @RequestHeader Map<String, String> map) {
-        log.info("生产者03 接收到的 id = {}", id);
-        log.info("生产者03 接收到的 x-key = {}", map.get("x-key"));
+        log.info("生产者04 接收到的 id = {}", id);
+        log.info("生产者04 接收到的 x-key = {}", map.get("x-key"));
         User user = userService.queryById(id);
         if (user != null) {
-            return Result.success("user-service-provider-03 查询成功~", user);
+            return Result.success("user-service-provider-04 查询成功~", user);
         } else {
             return Result.error("402", "id = " + id + " 的用户不存在");
         }
