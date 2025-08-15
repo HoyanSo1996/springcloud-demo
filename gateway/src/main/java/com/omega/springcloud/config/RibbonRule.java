5b7e69088c10da05a6ee33b152f53f328d4901bf
KennySo <hoyanso1996@163.com>
2025-08-15 12:00:00 +0800

    [Gateway] 配置 轮询算法.

---
+++ gateway/src/main/java/com/omega/springcloud/config/RibbonRule.java
@@ -0,0 +1,15 @@
+package com.omega.springcloud.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RibbonRule {
+
+    // /**
+    //  * 配置 负载均衡算法
+    //  */
+    // @Bean
+    // public IRule myRibbonRule() {
+    //     return new RandomRule();
+    // }
+}
