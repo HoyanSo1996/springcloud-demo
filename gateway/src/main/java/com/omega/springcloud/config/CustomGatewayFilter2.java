81c6719a1c406016191878fb910d8c4f19ab65a2
Kenny82005570 <Kenny.HX.Su@pccw.com>
2025-08-18 15:58:09 +0800

    [Gateway] 配置 全局过滤器.

---
+++ gateway/src/main/java/com/omega/springcloud/config/CustomGatewayFilter2.java
@@ -0,0 +1,28 @@
+package com.omega.springcloud.config;
+
+// @Order(0)
+// @Component
+// public class CustomGatewayFilter2 implements GlobalFilter {
+//
+//     /**
+//      * 处理当前请求。有必要的话通过{@link GatewayFilterChain}将请求交给下一个过滤器处理
+//      *
+//      * @param exchange  请求上下文，里面可以获取Request、Response等信息
+//      * @param chain  过滤器链，用来把请求委托给下一个过滤器
+//      * @return {@code Mono<Void>}  返回标示当前过滤器业务结束
+//      */
+//     @Override
+//     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+//         System.out.println("========== CustomGateWayFilter =========== ");
+//         String username = exchange.getRequest().getQueryParams().getFirst("username");
+//         String password = exchange.getRequest().getQueryParams().getFirst("password");
+//         if (!("kenny".equals(username) && "123456".equals(password))) {
+//             System.out.println("====== 非法用户 ======");
+//             exchange.getResponse().setStatusCode(HttpStatus.NOT_ACCEPTABLE);
+//             return exchange.getResponse().setComplete();
+//         }
+//         System.out.println("====== 验证通过, 放行 ======");
+//         return chain.filter(exchange);
+//     }
+//
+// }
